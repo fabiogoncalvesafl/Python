ec4c10711484cda0ad3fbe3bf42a5f3dfbd58a07
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-11-17 19:24:40 -0300

    add aula03

---
+++ aula03.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Fábio Gonçalves')
+print(1, 'Fábio "Gonçalves"')
+
+# Aspas duplas
+print("Fábio Gonçalves")
+print(2, "Fábio 'Gonçalves'")
+
+# Escape
+print("Fábio \"Gonçalves\"")
+
+# r
+print(r"Fábio \"Gonçalves\"")
