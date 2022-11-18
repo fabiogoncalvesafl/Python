804432e3fa8cdeb542c9a0b994b42745db8e747a
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-11-18 17:18:43 -0300

    add aula09

---
+++ aula09.py
@@ -0,0 +1,28 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+#divisivel por 8
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+#verificar par e impar (0 == par | 1 == impar)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
