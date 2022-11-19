1478952dcd6ea3028dcbb6ea6b2dc4bd4c63f153
Fábio Gonçalves <fabiogoncalves22@gmail.com>
2022-11-19 13:28:24 -0300

    add aula12

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Fábio Gonçalves'
+altura = 1.75
+peso = 90
+imc = peso / (altura ** 2)
+
+# Fábio Gonçalves tem 1.75 de altura,
+# pesa 90 quilos e seu IMC é
+# imc
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilo e seu IMC é')
+print(imc)
